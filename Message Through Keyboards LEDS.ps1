92bd8e5f000661a5ad4e94ced15b166227252c13
TheOncher <40915911+TheOncher@users.noreply.github.com>
2023-02-04 20:32:39 +0200

    Send Secret Message Through Keyboards LEDS
    This is a script to help you send secret messages through the keyboard, NUM-LOCK, CAPS-LOCK, SCROLL-LOCK LEDS, the message will bea  binary format representation of ascii characters converted to octal base,
    For example The Message "Hello" -> Dec: 72 101 108 108 111 -> Octal: 110  145 154 154 157 -> Binary: `each char in each number will be turned to binary foramt and displayed through the keyboard LEDS with special seperators after each letter`

---
+++ Message Through Keyboards LEDS.ps1
@@ -0,0 +1,109 @@
+# Parameters
+Param(
+    [Parameter(Mandatory = $True, HelpMessage = "Enter the sentence to be written with LEDs")]
+    [string]$Sentence,
+    [Parameter(Mandatory = $False, HelpMessage = "How much time the LEDs will blink before sentence")]
+    [int]$BlinkTime = 1,
+    [Parameter(Mandatory = $False, HelpMessage = "Enable Vertbosity to see what is the octal value for each letter")]
+    [switch]$VerboseOutput
+)
+
+# Reseting all keyboard LEDs
+function Reset-LEDS {
+    if ([System.Windows.Forms.Control]::IsKeyLocked('NumLock'))
+    { $keyboard.SendKeys("{NUMLOCK}") }
+    if ([System.Windows.Forms.Control]::IsKeyLocked('CapsLock'))
+    { $keyboard.SendKeys("{CAPSLOCK}") }
+    if ([System.Windows.Forms.Control]::IsKeyLocked('Scroll'))
+    { $keyboard.SendKeys("{SCROLLLOCK}") }
+}
+
+# Getting Users Attention
+function Blink-LEDS {
+    Start-Sleep -Seconds 0.5
+    for ($i = 0; $i -lt ($BlinkTime * 10) ; $i++) {
+        $keyboard.SendKeys("{NUMLOCK}")
+        $keyboard.SendKeys("{CAPSLOCK}")
+        $keyboard.SendKeys("{SCROLLLOCK}")
+        Start-Sleep(0.1)
+    }
+    # Safety mechanism
+    Reset-LEDS
+    Start-Sleep -Seconds 0.5
+}
+
+# End of letter blinking
+function EndLetter-LEDS{
+    Reset-LEDS
+    for ($i = 0; $i -lt 4; $i++){
+        $keyboard.SendKeys("{NUMLOCK}"); Start-Sleep -Seconds 0.3
+        $keyboard.SendKeys("{CAPSLOCK}"); Start-Sleep -Seconds 0.3
+        $keyboard.SendKeys("{SCROLLLOCK}"); Start-Sleep -Seconds 0.3
+    }
+    Reset-LEDS
+}
+
+# --------------------------------------------------------
+
+# Creating the keyboard object
+$keyboard = New-Object -ComObject WScript.Shell
+# Adding System.Windows.Forms Assembly
+Add-Type -AssemblyName System.Windows.Forms
+
+# Saving current keyboard state
+$NUM_STATE = [System.Windows.Forms.Control]::IsKeyLocked('NumLock')
+$CAPS_STATE = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
+$SCROLL_STATE = [System.Windows.Forms.Control]::IsKeyLocked('Scroll')
+
+# Reseting all keyboard LEDs
+# Getting Users Attention and starting sentence
+Reset-LEDS
+Blink-LEDS
+
+# Sending encrypted sentence via the keyboard LEDs
+for ($i = 0; $i -lt $Sentence.Length; $i++)
+{
+    EndLetter-LEDS
+    # Converting key to octal number
+    $Key = [byte][char]$Sentence[$i]
+    $Key = [Convert]::ToString($Key,8)
+    $KeyOctal = @()
+    $KeyOctal += [Math]::Floor($Key/100)
+    $KeyOctal += [Math]::Floor($Key/10)%10
+    $KeyOctal += $Key%10
+    if ($VerboseOutput.IsPresent)
+    {Write-Host $KeyOctal}
+
+    # Looping through all number in key
+    foreach ($Number in $KeyOctal)
+    {
+        $KeyOctalInBinary = [Convert]::ToString($Number,2)
+
+        $IsZero = $True
+        if (([Math]::Floor($KeyOctalInBinary/100)) -eq 1)
+        { $keyboard.SendKeys("{NUMLOCK}"); $NotZero = $False }
+        if (([Math]::Floor($KeyOctalInBinary/10)%10) -eq 1)
+        { $keyboard.SendKeys("{CAPSLOCK}"); $NotZero = $False }
+        if (($KeyOctalInBinary%10) -eq 1)
+        { $keyboard.SendKeys("{SCROLLLOCK}"); $NotZero = $False }
+
+        if ($IsZero -eq $True) {Start-Sleep -Seconds 1}
+        Start-Sleep -Seconds 0.5
+        Reset-LEDS
+        Start-Sleep -Seconds 0.5
+    }
+}
+EndLetter-LEDS
+
+# Reseting all keyboard LEDs
+# Getting Users Attention and ending sentence
+Reset-LEDS
+Blink-LEDS
+
+# Returning original keyboard state
+if ($NUM_STATE)
+{ $keyboard.SendKeys("{NUMLOCK}") }
+if ($CAPS_STATE)
+{ $keyboard.SendKeys("{CAPSLOCK}") }
+if ($SCROLL_STATE)
+{ $keyboard.SendKeys("{SCROLLLOCK}") }
